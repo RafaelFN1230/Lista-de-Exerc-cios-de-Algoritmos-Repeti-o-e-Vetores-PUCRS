cfba20ed31becd59d991b64e5a99d5030c72ba59
Rafael Figliuolo Nascimento <rafaelfnascimento1230@gmail.com>
2022-10-02 19:17:24 -0300

    Atualização 02/10/2022

--- 12.py
+++ 12.py
@@ -1,8 +1,16 @@
 '''
-12. Escrever um algoritmo que leia 20 valores para uma variável n e, para cada um deles, calcule a tabuada de 1 até n. Mostre a tabuada na forma:
+12. Escrever um algoritmo que leia 20 valores para uma variável n e, para cada um deles, 
+calcule a tabuada de 1 até n. Mostre a tabuada na forma:
 1 x n = n
 2 x n = 2n
 3 x n = 3n
 .......
 n x n = n2
 '''
+for x in range(20):
+    n=int(input("Insira o valor de N: "))
+    cont=0
+    for y in range(1,n+1):
+        total=y*n
+        cont+=1
+        print("%d X %d = "%(cont,n),total)
--- 13.py
+++ 13.py
@@ -1,4 +1,16 @@
 '''
-13. Escrever um algoritmo que leia um número n que indica quantos valores devem ser lidos a seguir. Para cada número lido, mostre uma tabela contendo o valor lido e o
+13. Escrever um algoritmo que leia um número n que indica quantos valores devem ser lidos a seguir. 
+Para cada número lido, mostre uma tabela contendo o valor lido e o
 fatorial deste valor.
 '''
+
+n=int(input("Insira quantos numeros devem ser lidos: "))
+
+for x in range(n):
+    a=int(input("Insira o valor do fatorial: "))
+    resp=a
+    for y in range (2,a+1):
+        resp*=y-1
+    print("Valor lido:",a)
+    print("Fatorial de %d:"%a,resp)
+
